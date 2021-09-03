6bcbdf7545f602094c155c2c8bad224d4a059809
ulug <ulgsds124@mail.ru>
2021-09-03 18:35:37 +0500

    sdasd

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+const user = {
+
+}
+
+for (let i = 1; i < 4; i++) {
+    let name = prompt("Ismingizni kiriting");
+    let age = prompt("Yoshingizni kiriting");
+     user.name = name;
+     user.age = age; 
+     console.log(`Foydalanuvchi ${i}: Ismi ${user.name}. Yoshi ${user.age} da.`);
+}
+
+
+
+
+
+
+
